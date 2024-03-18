8752fa52cbfe240fc3bd1456b974ab79af98b320
dany536 <deepankar.advisior@gmail.com>
2024-03-18 18:26:39 +0530

    feacture section

--- src/Container/Header/header.jsx
+++ src/Container/Header/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Link, NavLink} from 'react-router-dom'
+import { ChevronDown } from 'lucide-react';
 
 export default function Header() {
     return (
@@ -11,18 +12,12 @@ export default function Header() {
                         <h1 className="text-center text-xl font-serif">OPULENCE OASIS</h1>
                     </Link>
 
-                    <div className="flex items-center lg:order-2">
+                    <div className="flex  flex-row items-center lg:order-2">
                         <Link
                             to="#"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-black font-normal rounded-sm text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 border inline-flex"
                         >
-                            Log in
-                        </Link>
-                        <Link
-                            to="#"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                        >
-                            Get started
+                            TRAVEL  <ChevronDown className="ms-2 p-1 items-center" />
                         </Link>
                     </div>
 
